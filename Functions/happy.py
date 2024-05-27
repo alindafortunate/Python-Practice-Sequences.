6fe1067da557679c7a0bed457e8c02ae7132fbea
alindafortunate <alindafortunate5@gmail.com>
2024-05-27 09:21:41 +0300

    Chapter 6 Functions

---
+++ Functions/happy.py
@@ -0,0 +1,17 @@
+# happy.py
+# A program to print the lyrics of a happy birthday song.
+# defining functions
+
+
+def happy():
+    print("Happy Birthday to you!")
+
+
+def singFred():
+    happy()
+    happy()
+    print("Happy Birthday to you, dear Fred")
+    happy()
+
+
+singFred()
